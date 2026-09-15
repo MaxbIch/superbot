0c735fd91b87519bb075aca2394ad357616f7e23
MaxbIch <94921982+MaxbIch@users.noreply.github.com>
2026-09-15 09:53:29 +0700

    Scroll to top on every page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import TransportPage from "./pages/TransportPage";
@@ -11,9 +12,20 @@ import PlacesPage from "./pages/PlacesPage";
 import PlaceDetailPage from "./pages/PlaceDetailPage";
 import ReviewsPage from "./pages/ReviewsPage";
 
+function ScrollToTop() {
+  const { pathname, search } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname, search]);
+
+  return null;
+}
+
 export default function App() {
   return (
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/transport" element={<TransportPage />} />
