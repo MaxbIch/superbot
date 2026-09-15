39fb5920a4436f051853af3840aa42027550b04a
MaxbIch <94921982+MaxbIch@users.noreply.github.com>
2026-09-15 16:24:56 +0700

    fix: add Telegram initData to shared WebApp type

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -91,6 +91,7 @@ declare global {
     interface Window {
         Telegram?: {
             WebApp?: {
+                initData?: string;
                 initDataUnsafe?: {
                     user?: TelegramUserInfo;
                 };
